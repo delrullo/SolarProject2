ed240edb81906d2cbbb2d8961494c860b66ebe71
meroebeigi <meroeasgharbeigi00@gmail.com>
2023-11-09 15:14:16 +0100

    first question

--- solar.py
+++ solar.py
@@ -10,8 +10,8 @@ from datetime import timedelta
 from solarfun import (calculate_B_0_horizontal,
                       calculate_G_ground_horizontal,                      
                       calculate_diffuse_fraction,
-                      calculate_incident_angle,
-                      calculate_G_0_horizontal)
+                      calculate_incident_angle
+                      )
 
 
 # tilt representes inclination of the solar panel (in degress), orientation
@@ -47,7 +47,9 @@ timeseries['B_0_h'] = calculate_B_0_horizontal(hours, hour_0, lon, lat)
 timeseries['K_t']=0.7*np.ones(len(hours))  
 
 # Timeseries G_zero
-timeseries['G_0_h'] = calculate_G_0_horizontal(K_t,B_0_h)
+timeseries['G_0_h'] = timeseries['K_t'] * timeseries['B_0_h']
+
+
 
 # Calculate global horizontal irradiance on the ground
 [timeseries['G_ground_h'], timeseries['solar_altitude']] = calculate_G_ground_horizontal(hours, hour_0, lon, lat, timeseries['K_t'])
@@ -59,7 +61,35 @@ timeseries['F'] = calculate_diffuse_fraction(hours, hour_0, lon, lat, timeseries
 timeseries['B_ground_h']=[x*(1-y) for x,y in zip(timeseries['G_ground_h'], timeseries['F'])]
 timeseries['D_ground_h']=[x*y for x,y in zip(timeseries['G_ground_h'], timeseries['F'])]
 
-# plot
+
+# Create a subplot with 2 rows and 1 column
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10))
+
+# Plotting G_0_h time series for the first week of June
+ax1.plot(timeseries['G_0_h']['2018-06-01 00:00':'2018-06-08 00:00'], label='G_0_h (June)', color='green')
+ax1.set_title('G_0_h Time Series (June 1st - June 8th)')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('G_0_h')
+ax1.legend()
+ax1.grid(True)
+
+# Plotting G_0_h time series for the first week of February
+ax2.plot(timeseries['G_0_h']['2018-02-01 00:00':'2018-02-08 00:00'], label='G_0_h (February)', color='blue')
+ax2.set_title('G_0_h Time Series (Feb 1st - Feb 8th)')
+ax2.set_xlabel('Time')
+ax2.set_ylabel('G_0_h')
+ax2.legend()
+ax2.grid(True)
+
+# Adjust layout for better spacing
+plt.tight_layout()
+
+# Show the plot
+plt.show()
+
+
+
+
 plt.figure(figsize=(20, 10))
 gs1 = gridspec.GridSpec(2, 2)
 #gs1.update(wspace=0.3, hspace=0.3)
@@ -72,3 +102,4 @@ ax1.plot(timeseries['D_ground_h']['2018-06-01 00:00':'2018-06-08 00:00'],
          label='D_ground_h', color= 'purple')
 ax1.legend(fancybox=True, shadow=True,fontsize=12, loc='best')
 ax1.set_ylabel('W/m2')
+
